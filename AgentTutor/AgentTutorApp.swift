c8a27fd3d0db8a1cdcb4e1e95148a85841a9b2d6
Kai <kai@thekaiway.com>
2026-02-09 01:04:42 +0800

    Initial Commit

---
+++ AgentTutor/AgentTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgentTutorApp.swift
+//  AgentTutor
+//
+//  Created by Kai on 2026-02-09.
+//
+
+import SwiftUI
+
+@main
+struct AgentTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AgentTutorTests/AgentTutorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AgentTutorTests.swift
+//  AgentTutorTests
+//
+//  Created by Kai on 2026-02-09.
+//
+
+import Testing
+@testable import AgentTutor
+
+struct AgentTutorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
